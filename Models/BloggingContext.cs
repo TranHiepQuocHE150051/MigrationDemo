76fe20b52d00050709211b2669e8dc54b13987ca
TrầnHiệpQuốcHE150051 <91178389+TranHiepQuocHE150051@users.noreply.github.com>
2023-06-08 17:08:17 +0700

    Initial commit

---
+++ Models/BloggingContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationDemo.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(local);Database=Blogging;Trusted_Connection=True;Encrypt=false");
+        }
+
+    }
+}
